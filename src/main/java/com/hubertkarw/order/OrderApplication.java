290d31fcf160827d5e4e3ad26a11c30c0564503a
Hubert.Karw <hubert_karwowski@o2.pl>
2025-09-11 22:41:41 +0200

    task3/first commit

---
+++ src/main/java/com/hubertkarw/order/OrderApplication.java
@@ -0,0 +1,13 @@
+package com.hubertkarw.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApplication.class, args);
+	}
+
+}
